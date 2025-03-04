ff8fb569b587a316fb57e73623cf64b187ed95e0
i218 <i852086@gmail.com>
2025-03-04 07:57:01 +0200

    Added /start message and fixed @ text

--- GetSenderId_TgBot/Program.cs
+++ GetSenderId_TgBot/Program.cs
@@ -14,6 +14,7 @@ namespace GetSenderId_TgBot
             bot.Init();
 
             try { Task.Delay(-1).GetAwaiter().GetResult(); } catch (Exception e) { Console.WriteLine(e.Message); }
+            Console.ReadLine();
         }
     }
 
@@ -34,11 +35,24 @@ namespace GetSenderId_TgBot
 
         private async Task UpdateHandler(ITelegramBotClient arg1, Update updateArg, CancellationToken cancellationArg)
         {
-            if (updateArg.Type is not UpdateType.Message || updateArg.Message?.ForwardOrigin is null) return;
             var msg = updateArg.Message;
+            if ((updateArg.Type is not UpdateType.Message || msg?.ForwardOrigin is null) && msg is not {Text: "/start"}) return;
+
             var chat = msg.Chat;
 
-            switch (msg.ForwardOrigin.Type)
+            const string startText = "Hi there, this bot does literally what name says - he gives u the ID of user message was forwarded from." +
+                                     "\r\n\r\nIt won't make logs, or reply to unforwarded messages, feel free to use it as an storage \ud83d\ude01" +
+                                     "\r\n\r\nIt's also an open-source, u can find code <a href='https://github.com/i218/GetSenderId_TgBot'>here</a>.";
+
+            var options = new LinkPreviewOptions { IsDisabled = true };
+
+            if (msg.Text == "/start")
+            {
+                await _client.SendMessage(chat, startText, parseMode: ParseMode.Html, linkPreviewOptions: options, cancellationToken: cancellationArg);
+                return;
+            }
+
+            switch (msg.ForwardOrigin?.Type)
             {
                 case MessageOriginType.HiddenUser:
                     await _client.SendMessage(chat, "User has <b>disabled \'forwarding\'</b> in settings, getting id is impossible", ParseMode.Html, cancellationToken: cancellationArg);
@@ -69,7 +83,7 @@ namespace GetSenderId_TgBot
             if (msg.ForwardFrom is null) return;
             var idText = msg.ForwardFrom.Id;
             var username = msg.ForwardFrom.Username;
-            var usernameText = username is null ? "" : $"\n<b>@:</b> @{username}";
+            var usernameText = username is null ? "" : $"\n<b>UserTag:</b> @{username}";
             await _client.SendMessage(chat, $"Forwarded from: \n<b>User Id:</b> <code>{idText}</code> {usernameText}", ParseMode.Html);
         }
 
